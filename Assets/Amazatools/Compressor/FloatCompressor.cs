04be03cc8ef1a190fe2ba44207ecfb7875a2e320
Anthony <anthony@amazastrophic.com>
2020-10-23 01:27:14 -0400

    Inital version
    Two scripts; one is the compressor and the other is the test.

---
+++ Assets/Amazatools/Compressor/FloatCompressor.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Amazatools.Compression
+{
+    public static class FloatCompressor
+    {
+        public static uint SimpleCompress(float value, int min, int max, float accuracy)
+        {
+            return Math.Min((uint)((value + -min) * (1.0f / accuracy)), (uint)((max - min) * (1.0 / accuracy)));
+        }
+
+        public static float SimpleDecompress(uint value, int min, int max, float accuracy)
+        {
+            return Clamp((value * accuracy) + min, min, max);
+        }
+
+        public static FloatCompression TotalCompression(float value, FloatCompressionSettings settings)
+        {
+            uint compressed = Math.Min((uint)((value + -settings.Min) * (1.0f / settings.Accuracy)),(uint)((settings.Max - settings.Min) * (1.0 / settings.Accuracy)));
+            return new FloatCompression(compressed,settings.Min,settings.Max,settings.Accuracy,(int)BitTotal(compressed));
+        }
+
+        public static float TotalUnCompression(FloatCompression compressed)
+        {
+            return Clamp((compressed._compressed * compressed._accuracy) + compressed._min, compressed._min, compressed._max);
+        }
+
+        public static uint BitTotal(uint value)
+        {
+            value |= value >> 1;
+            value |= value >> 2;
+            value |= value >> 4;
+            value |= value >> 8;
+            return DeBruijnLSBsSet[unchecked((value | value >> 16) * 0x07c4acddu) >> 27];
+        }
+
+        public static byte[] DeBruijnLSBsSet = new byte[] { 0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30, 8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31 };
+
+        public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T>
+        {
+            if (val.CompareTo(min) < 0) return min;
+            else if (val.CompareTo(max) > 0) return max;
+            else return val;
+        }
+    }
+
+    [System.Serializable]
+    public struct FloatCompression
+    {
+        public readonly uint _compressed;
+        public readonly float _min;
+        public readonly float _max;
+        public readonly float _accuracy;
+        public readonly int _bits;
+
+        public FloatCompression(uint compressed, float min, float max, float accuracy, int bits)
+        {
+            this._compressed = compressed;
+            this._min = min;
+            this._max = max;
+            this._accuracy = accuracy;
+            this._bits = bits;
+        }
+    }
+
+    [System.Serializable]
+    public struct FloatCompressionSettings
+    {
+        public int Min;
+        public int Max;
+        public float Accuracy;
+    }
+}
---
+++ Assets/Amazatools/Compressor/Test/TestFloatCompression.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Amazatools.Compression
+{    /// <summary>
+     /// Place on a gameobject, fill out the test float, and then place two preview objects to test.
+     /// The two top lines inside UpdateTest are the meat.
+     /// </summary>
+    public class TestFloatCompression : MonoBehaviour
+    {
+       
+        public float TestFloat = 1234.567f;
+        public Transform PreviewUncompressed;
+        public Transform PreviewCubeCompressed;
+        [Header("Real time status:")]
+        public float TestFloatAfterDecompression;
+        public float CompressionDiffrence;
+        public int CompressedBits;
+        public FloatCompressionSettings TestCompressionSettings = new FloatCompressionSettings { Accuracy = 0.02f, Min = -5000, Max = 5000 };
+        private FloatCompression FloatCompression;
+
+
+        void Start()
+        {
+            Debug.Log("Starting Compression Test");
+            InvokeRepeating(nameof(UpdateTest), 1f, 1f);
+            Camera.main.transform.position= new Vector3(TestFloat, 0, -3);
+            PreviewUncompressed.gameObject.GetComponent<Renderer>().material.color = Color.red;
+            PreviewCubeCompressed.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+        }
+
+        void UpdateTest()
+        {
+            FloatCompression = FloatCompressor.TotalCompression(TestFloat, TestCompressionSettings);
+            TestFloatAfterDecompression = FloatCompressor.TotalUnCompression(FloatCompression);
+
+            if (TestCompressionSettings.Min < TestFloatAfterDecompression && TestFloatAfterDecompression < TestCompressionSettings.Max)
+            {
+                CompressionDiffrence = TestFloat - TestFloatAfterDecompression;
+                CompressedBits = FloatCompression._bits;
+                Debug.Log("Uncompressed float was:" + TestFloat + " | Compressed uint: " + FloatCompression._compressed + " | Decompressed float: " + TestFloatAfterDecompression);
+                Debug.Log("This was a difference of:" + CompressionDiffrence + " | This is a total bit size of:" + CompressedBits);
+                PreviewUncompressed.position = new Vector3(TestFloat, 0,0);
+                PreviewCubeCompressed.position = new Vector3(TestFloatAfterDecompression, 0, 0);
+            }
+            else
+            {
+                if(TestCompressionSettings.Min > TestFloatAfterDecompression)
+                Debug.LogWarning("The TestFloat is lower then TestCompressionSettings.Min here is the result:" + TestFloatAfterDecompression);
+                else
+                Debug.LogWarning("The TestFloat is higher then TestCompressionSettings.mac here is the result:" + TestFloatAfterDecompression);
+            }
+        }
+    }
+}
