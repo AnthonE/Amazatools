42685f362082d40115ebbdc863c7444675cb91e6
Anthony Eckert <anthony@amazastrophic.com>
2020-11-20 15:43:29 -0500

    Update FloatCompressor.cs
    The old BitTotal method would give one less then it should have. Thanks to a discussion of this discovery on Mirror's discord, FSE_Vincenzo hooked me up with the right code.

--- Assets/Amazatools/Compressor/FloatCompressor.cs
+++ Assets/Amazatools/Compressor/FloatCompressor.cs
@@ -17,25 +17,28 @@ namespace Amazatools.Compression
         public static FloatCompression TotalCompression(float value, FloatCompressionSettings settings)
         {
             uint compressed = Math.Min((uint)((value + -settings.Min) * (1.0f / settings.Accuracy)),(uint)((settings.Max - settings.Min) * (1.0 / settings.Accuracy)));
-            return new FloatCompression(compressed,settings.Min,settings.Max,settings.Accuracy,(int)BitTotal(compressed));
+            return new FloatCompression(compressed,settings.Min,settings.Max,settings.Accuracy,Log2(compressed));
         }
 
         public static float TotalUnCompression(FloatCompression compressed)
         {
             return Clamp((compressed._compressed * compressed._accuracy) + compressed._min, compressed._min, compressed._max);
         }
-
-        public static uint BitTotal(uint value)
+        
+        public static int Log2(uint x)
         {
-            value |= value >> 1;
-            value |= value >> 2;
-            value |= value >> 4;
-            value |= value >> 8;
-            return DeBruijnLSBsSet[unchecked((value | value >> 16) * 0x07c4acddu) >> 27];
+            x |= x >> 1;
+            x |= x >> 2;
+            x |= x >> 4;
+            x |= x >> 8;
+            x |= x >> 16;
+            x = x >> 1;
+            x = x - ((x >> 1) & 0x55555555);
+            x = (x & 0x33333333) + ((x >> 2) & 0x33333333);
+            x = ((x + (x >> 4) & 0xF0F0F0F) * 0x1010101) >> 24;
+            return (int)x+1;
         }
-
-        public static readonly byte[] DeBruijnLSBsSet = new byte[] { 0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30, 8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31 };
-
+        
         public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T>
         {
             if (val.CompareTo(min) < 0) { return min; }
