0652ca7cb51487933f68a064bc36e3f74b61dd91
Anthony Eckert <anthony@amazastrophic.com>
2020-11-20 16:08:10 -0500

    Update FloatCompressor.cs
    Added additional SimpleDecompress method that takes a FloatCompressionSettings

--- Assets/Amazatools/Compressor/FloatCompressor.cs
+++ Assets/Amazatools/Compressor/FloatCompressor.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Amazatools.Compression
 {
@@ -14,17 +14,22 @@ namespace Amazatools.Compression
             return Clamp((value * accuracy) + min, min, max);
         }
 
-        public static FloatCompression TotalCompression(float value, FloatCompressionSettings settings)
+        public static float SimpleDecompress(uint value, FloatCompressionSettings settings)
         {
-            uint compressed = Math.Min((uint)((value + -settings.Min) * (1.0f / settings.Accuracy)),(uint)((settings.Max - settings.Min) * (1.0 / settings.Accuracy)));
-            return new FloatCompression(compressed,settings.Min,settings.Max,settings.Accuracy,Log2(compressed));
+            return Clamp((value * settings.Accuracy) + settings.Min, settings.Min, settings.Max);
         }
 
-        public static float TotalUnCompression(FloatCompression compressed)
+        public static FloatCompression TotalCompression(float value, FloatCompressionSettings settings)
+        {
+            uint compressed = Math.Min((uint)((value + -settings.Min) * (1.0f / settings.Accuracy)), (uint)((settings.Max - settings.Min) * (1.0 / settings.Accuracy)));
+            return new FloatCompression(compressed, settings.Min, settings.Max, settings.Accuracy, Log2(compressed));
+        }
+
+        public static float TotalDecompression(FloatCompression compressed)
         {
             return Clamp((compressed._compressed * compressed._accuracy) + compressed._min, compressed._min, compressed._max);
         }
-        
+
         public static int Log2(uint x)
         {
             x |= x >> 1;
@@ -36,19 +41,19 @@ namespace Amazatools.Compression
             x = x - ((x >> 1) & 0x55555555);
             x = (x & 0x33333333) + ((x >> 2) & 0x33333333);
             x = ((x + (x >> 4) & 0xF0F0F0F) * 0x1010101) >> 24;
-            return (int)x+1;
+            return (int)x + 1;
         }
-        
+
         public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T>
         {
             if (val.CompareTo(min) < 0) { return min; }
             else if (val.CompareTo(max) > 0) { return max; }
-            else { return val; }          
+            else { return val; }
         }
     }
-     
-    [System.Serializable]
-    public struct FloatCompression  : IEquatable<FloatCompression>
+
+    [Serializable]
+    public struct FloatCompression : IEquatable<FloatCompression>
     {
         public readonly uint _compressed;
         public readonly float _min;
@@ -71,12 +76,12 @@ namespace Amazatools.Compression
             {
                 return true;
             }
-                return false;
+            return false;
         }
 
     }
 
-    [System.Serializable]
+    [Serializable]
     public struct FloatCompressionSettings
     {
         public int Min;
